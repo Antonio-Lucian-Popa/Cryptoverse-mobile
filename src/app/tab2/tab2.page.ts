8b36bea7b2b8125fecaba4d5e45e7ee4862ab2e9
Asu Software <antonio.popa.dev@gmail.com>
2022-11-08 23:16:22 +0200

    Added search logic

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { IonModal } from '@ionic/angular';
 import { CoinService } from '../shared/services/coin.service';
 
@@ -16,12 +17,32 @@ export class Tab2Page implements OnInit {
   cryptos: any[] = [];
   currentCrypto: any;
 
-  constructor(private coinService: CoinService) {}
+  userForm = this.fb.group({
+    searchWord: ['']
+  });
+
+  filteredCrypto: any[] = [];
+
+  constructor(private coinService: CoinService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.coinService.getCryptos(100).subscribe(res => {
       this.cryptos = res.data.coins;
+      this.filteredCrypto = res.data.coins;
     });
+    this.userForm.get('searchWord').valueChanges.subscribe(word => {
+      this.searchCrypto(word);
+    });
+  }
+
+  searchCrypto(wordToSearch: string): void {
+    this.filteredCrypto = this.cryptos.filter(crypto => crypto.name.toLocaleLowerCase().includes(wordToSearch.toLocaleLowerCase())
+    || crypto.symbol.toLocaleLowerCase().includes(wordToSearch.toLocaleLowerCase())
+    );
+    console.log(this.filteredCrypto);
+    if(!wordToSearch) {
+      this.filteredCrypto = this.cryptos;
+    }
   }
 
   openDialog(currentCrypto: any) {
